0ab32952df8fc02bb6c3b4e5a2ec61994b2d8673
murad01 <adylovmurad@gmail.com>
2026-03-30 14:33:14 +0500

    Fixed bugs

--- src/main/java/ru/murad/NauJava/service/AuthorServiceImpl.java
+++ src/main/java/ru/murad/NauJava/service/AuthorServiceImpl.java
@@ -2,13 +2,12 @@ package ru.murad.NauJava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 import ru.murad.NauJava.entity.Author;
 import ru.murad.NauJava.entity.Book;
 import ru.murad.NauJava.repository.AuthorRepository;
 import ru.murad.NauJava.repository.BookRepository;
+import ru.murad.NauJava.exception.ResourceNotFoundException;
 
 import java.util.List;
 
@@ -17,37 +16,23 @@ public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
-    private final PlatformTransactionManager transactionManager;
 
     @Autowired
     public AuthorServiceImpl(AuthorRepository authorRepository,
-                             BookRepository bookRepository,
-                             PlatformTransactionManager transactionManager) {
+                             BookRepository bookRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
-        this.transactionManager = transactionManager;
     }
 
     @Override
+    @Transactional
     public void deleteAuthorWithBooks(Long authorId) {
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new ResourceNotFoundException("Автор не найден с ID: " + authorId));
 
-        try {
-            Author author = authorRepository.findById(authorId)
-                    .orElseThrow(() -> new RuntimeException("Автор не найден с ID: " + authorId));
+        List<Book> authorBooks = bookRepository.findByAuthorFullName(author.getFullName());
 
-            List<Book> authorBooks = bookRepository.findByAuthorFullName(author.getFullName());
-
-            bookRepository.deleteAll(authorBooks);
-            authorRepository.deleteById(authorId);
-
-            transactionManager.commit(status);
-            System.out.println("Транзакция завершена успешно.");
-
-        } catch (RuntimeException ex) {
-            transactionManager.rollback(status);
-            System.err.println("Транзакция откачена: " + ex.getMessage());
-            throw ex;
-        }
+        bookRepository.deleteAll(authorBooks);
+        authorRepository.delete(author);
     }
 }
